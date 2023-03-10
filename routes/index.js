59807434c938e708e9f1d648105fa00c0d941e0c
Samyaksam17 <jsamyak407@gmail.com>
2023-03-10 10:27:50 +0530

    Updated :gun:

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 router.get('/ping', (req, res)=>{
     res.json({response: "pong", details: {project_name: "Zapit", version: "0.0.1", tag: "demo", description: "Zapit assesment ..." } });
